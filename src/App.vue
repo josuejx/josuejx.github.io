57931f8f3a7f5c6a5610b85ede7429c4b8f8861b
josuejx <josue.ga00@gmail.com>
2026-06-13 18:05:33 +0200

    feat: app header

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
+import AppHeader from './components/layout/AppHeader.vue';
+
 </script>
 
 <template>
+    <AppHeader />
     <h1>Welcome to Your Vue.js App</h1>
 </template>
